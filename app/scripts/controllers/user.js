fe350566edcbd06ddf945b44a825a88e901bac77
lizkline91 <lizkline91@gmail.com>
2014-10-08 13:01:19 -0400

    added user stack
    individual view of current user collection with grifter

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -1,33 +1,37 @@
 'use strict';
-
 /**
  * @ngdoc function
- * @name stacksonstacksApp.controller:UserCtrl
+ * @name stacksonstacksApp.controller:ChatCtrl
  * @description
- * # UserCtrl
- * Controller of the stacksonstacksApp
+ * # ChatCtrl
+ * A demo of using AngularFire to manage a synchronized list.
  */
 angular.module('stacksonstacksApp')
-  .controller('UserCtrl',['$scope', '$rootScope', '$firebaseSimpleLogin', '$location', 'fbutil','$routeParams', function ($scope, $rootScope, $firebaseSimpleLogin, $location, fbutil, $routeParams, $log) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-
+  .controller('UserCtrl', function ($firebaseSimpleLogin, $rootScope, $scope, simpleLogin, fbutil, $timeout) {
+    // synchronize a read-only, synchronized array of messages, limit to most recent 10
 
     var auth = $firebaseSimpleLogin(fbutil.ref());
     auth.$getCurrentUser().then(function (user) {
         $rootScope.currentUser = user;
+
     });
+
 
     $scope.users = fbutil.syncArray('users');
 
-    $scope.users.currentUser.collections = fbutil.syncArray('collections');
-    $scope.addSomething = function (something) {
-      var ref = fbutil.ref('users');
-      ref.child($rootScope.currentUser.uid).child('collections').push(something);
+
+    function loadProfile(user) {
+      if( $scope.profile ) {
+        $scope.profile.$destroy();
+      }
+      fbutil.syncObject('users/'+user.uid).$bindTo($scope, 'profile');
     }
+  });
 
+  $scope.users = fbutil.syncArray('users');
 
-}]);
+  $scope.users.currentUser.collections = fbutil.syncArray('collections');
+  $scope.addSomething = function (something) {
+    var ref = fbutil.ref('users');
+    ref.child($rootScope.currentUser.uid).child('collections').push(something);
+  };
